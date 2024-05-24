089dfdc2dbade106016556fc169c0f407c6f7bc9
BONAFO <91232074+BONAFO@users.noreply.github.com>
2024-05-24 15:55:23 -0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,158 +3,27 @@
 </template>
 
 <script setup>
-import { convertToBase, convertFromBase, seed } from "@/assets/zip";
-const toBinary = (value) => {
-  let binary = "";
+import {   } from "@/assets/zip";
 
-  if (!Number.isInteger(value)) {
-    for (let i = 0; i < value.length; i++) {
-      let ascii = value.charCodeAt(i).toString(2);
-      binary += "0".repeat(8 - ascii.length) + ascii;
-    }
-  } else {
-    binary = value.toString(2);
-  }
-
-  return binary;
-};
-
-const toHexa = () => {};
-
-const binToBig = (binary) => {
-  // console.log(convertToBase(parseInt(binary, 2)).length);
-  // console.log(parseInt(binary, 2).toString(35));
-  // return convertToBase(parseInt(binary, 2))
-  // return convertToBase(parseInt(binary, 2))
-  return BigInt("0b" + binary).toString(16);
-};
-const bigToBin = (bint) => {
-  // return BigInt(bint)
-  // // console.log(convertToBase(parseInt(binary, 2)).length);
-  // // console.log(parseInt(binary, 2).toString(35));
-  // // return convertToBase(parseInt(binary, 2))
-  // // return convertToBase(parseInt(binary, 2))
-  // return BigInt(parseInt(binary, 2)).toString(36)
-  return BigInt("0x" + bint);
-};
-
-const binToString = (binary) => {
-  let word = "";
-  for (let i = 0; i < binary.length; i += 8) {
-    word += String.fromCharCode(parseInt(binary.substr(i, 8), 2));
-  }
-  return word;
-};
-
-// const zip_binary =(binary)=>{
-//     let ziped = "";
-//     for (let i = 0; i < binary.length; i++) {
-//         console.log(binary[i]);
-
-//     }
-// }
-
-const calculate_base = (binary) => {
-  let max_pow = 0;
-
-  do {
-    if (Math.pow(2, max_pow) >= seed.length) {
-      if (Math.pow(2, max_pow) > seed.length) {
-        max_pow--;
-      }
-      break;
-    }
-    max_pow++;
-  } while (true);
-
-  if (binary <= max_pow) {
-    return binary;
-  } else {
-    return max_pow;
-  }
-};
-
-const generate_table = (HEAD) => {
-  let table = [];
-  let equi = [];
-  for (let i = 0; i < Math.pow(2, HEAD); i++) {
-    table.push({ val: seed[i], bin: i.toString(2).padStart(HEAD, "0") });
-  }
-  return table;
-};
-
-const zip_rest = (rest) => {
-  return parseInt(rest, 2);
-};
-
-const zip_binary = (binary, head) => {
-  const table = generate_table(head);
-  let max = Math.floor(binary.length / head);
-  let token = "";
-  let rest = "";
-
-  for (let i = 0; i < max; ++i) {
-    token += table.filter(
-      (b) => b.bin == binary.substring(i * head, (i + 1) * head)
-    )[0].val;
-  }
-
-  rest = binary.substring(max * head, binary.length);
-
-  if (rest.length != 0) {
-    rest = zip_binary(rest, rest.length)
-      .replaceAll("*", "")
-      .replaceAll("@", "")
-      .split("|");
-    rest = `${rest[0]}:${rest[2]}`;
-  }
-  token += "@" + rest;
-
-  //   console.log(zip_binary());
-
-  return `*${head}|${max}|${token}*`;
-};
-
-const unzip_rest = (rest) => {
-  const [head, cont] = rest.split(":");
-  const table = generate_table(head);
-return table.filter((b) => b.val == cont)[0].bin;
-
-};
-
-const unzip_binary = (token) => {
-  const [head, max, content] = token.split("|");
-  const [dataco, rest] = content.split("@");
-  const table = generate_table(head);
-  let binary = "";
-  for (let i = 0; i < dataco.length; ++i) {
-    // token += table.filter(b => b.bin == binary.substring(i * head, (i + 1) * head))[0].val;
-    binary += table.filter((b) => b.val == dataco[i])[0].bin;
-  }
-   if(rest.length != 0){
-    binary += unzip_rest(rest)
-   }
-
-  return binary;
-};
+// const string = "{@a@: 'Wendigo'}"
+// const ZIPPED = build_url(string);
+// const UNZIPED = decode_url(ZIPPED);
+// console.log(string == UNZIPED);
 
 // console.log(toBinary("Wendigo"));
 
 //DE STR A BINARIO
 
-const string = "C123o'12i39'ick1o'2ui34";
-const A = toBinary(string);
+// const string = "{@a@: 'Wendigo'}";
+// const A = toBinary(string);
 
-// console.clear();
+// // console.clear();
 
-const HEAD = calculate_base(A.length);
-const ZIPPED = zip_binary(A, HEAD);
+// const HEAD = calculate_base(A.length);
+// const ZIPPED = zip_binary(A, HEAD);
 
-const UNZIPED = unzip_binary(ZIPPED.replaceAll("*", ""));
-const palabra = binToString(UNZIPED);
-
-console.log(string == palabra);
-
+// const UNZIPED = unzip_binary(ZIPPED.replaceAll("*", ""));
+// const palabra = binToString(UNZIPED);
 
 
 // ESTRUCTURA TOKEN
--- src/assets/zip.js
+++ src/assets/zip.js
@@ -17,118 +17,155 @@ const genen_seed_zip = () => {
   seed = seed.concat(seed3);
 };
 
-// const genen_string_seed_zip = () => {
-//   let s = "qwertyuiopasdfghjklzxcvbnm ".split("");
-
-//   let seed2 = [];
-//   let seed = [];
-
-//   for (let i = 0; i < s.length; i++) {
-//     seed2.push(s[i]);
-//     seed2.push(s[i].toUpperCase());
-//   }
-
-//   let breaker = true;
-//   let counter = 0;
-//   let i = 0;
-//   do {
-//     if (i % 10 == 0 || i == 33) {
-//       seed.push(null);
-//     } else if (seed2[counter] != undefined) {
-//       seed.push(seed2[counter]);
-//       counter++;
-//     } else {
-//       break;
-//     }
-//     i++;
-//   } while (breaker);
-
-//   seed[32] = null;
-//   string_seed = string_seed.concat(seed);
-// };
 
 const parseBoolArr = (bArr) => {
   return bArr.map((v) => (v ? 1 : 0));
 };
-// const parseBinary = (arr) => {
-//   if (typeof arr[0] == "boolean") {
-//     arr = parseBoolArr(arr);
+
+// export function convertToBase(number) {
+//   const base = seed.length; // Base 64
+//   let result = "";
+//   while (number > 0) {
+//     result = seed[number % base] + result;
+//     number = Math.floor(number / base);
 //   }
-//   arr = arr.join("");
-//   return parseInt(arr, 2).toString(16);
-// };
 
-export function convertToBase(number) {
-  const base = seed.length; // Base 64
-  let result = "";
-  while (number > 0) {
-    result = seed[number % base] + result;
-    number = Math.floor(number / base);
+//   return result || "0"; // Manejo del caso de número 0
+// }
+
+// export function convertFromBase(str) {
+//   const base = seed.length; // Base 64
+
+//   let result = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     const digitValue = seed.indexOf(str[i]);
+//     result = result * base + digitValue;
+//   }
+
+//   return result;
+// }
+
+
+const toBinary = (value) => {
+    let binary = "";
+  
+    if (!Number.isInteger(value)) {
+      for (let i = 0; i < value.length; i++) {
+        let ascii = value.charCodeAt(i).toString(2);
+        binary += "0".repeat(8 - ascii.length) + ascii;
+      }
+    } else {
+      binary = value.toString(2);
+    }
+  
+    return binary;
+  };
+  
+  
+  
+  const binToString = (binary) => {
+    let word = "";
+    for (let i = 0; i < binary.length; i += 8) {
+      word += String.fromCharCode(parseInt(binary.substr(i, 8), 2));
+    }
+    return word;
+  };
+  
+  
+  const calculate_base = (binary) => {
+    let max_pow = 0;
+  
+    do {
+      if (Math.pow(2, max_pow) >= seed.length) {
+        if (Math.pow(2, max_pow) > seed.length) {
+          max_pow--;
+        }
+        break;
+      }
+      max_pow++;
+    } while (true);
+  
+    if (binary <= max_pow) {
+      return binary;
+    } else {
+      return max_pow;
+    }
+  };
+  
+  const generate_table = (HEAD) => {
+    let table = [];
+    let equi = [];
+    for (let i = 0; i < Math.pow(2, HEAD); i++) {
+      table.push({ val: seed[i], bin: i.toString(2).padStart(HEAD, "0") });
+    }
+    return table;
+  };
+  
+  
+  const zip_binary = (binary, head) => {
+    const table = generate_table(head);
+    let max = Math.floor(binary.length / head);
+    let token = "";
+    let rest = "";
+  
+    for (let i = 0; i < max; ++i) {
+      token += table.filter(
+        (b) => b.bin == binary.substring(i * head, (i + 1) * head)
+      )[0].val;
+    }
+  
+    rest = binary.substring(max * head, binary.length);
+  
+    if (rest.length != 0) {
+      rest = zip_binary(rest, rest.length)
+        .replaceAll("*", "")
+        .replaceAll("@", "")
+        .split("|");
+      rest = `${rest[0]}:${rest[2]}`;
+    }
+    token += "@" + rest;
+  
+    //   console.log(zip_binary());
+  
+    return `*${head}|${max}|${token}*`;
+  };
+  
+  const unzip_rest = (rest) => {
+    const [head, cont] = rest.split(":");
+    const table = generate_table(head);
+  return table.filter((b) => b.val == cont)[0].bin;
+  
+  };
+  
+  const unzip_binary = (token) => {
+    const [head, max, content] = token.split("|");
+    const [dataco, rest] = content.split("@");
+    const table = generate_table(head);
+    let binary = "";
+    for (let i = 0; i < dataco.length; ++i) {
+      // token += table.filter(b => b.bin == binary.substring(i * head, (i + 1) * head))[0].val;
+      binary += table.filter((b) => b.val == dataco[i])[0].bin;
+    }
+     if(rest.length != 0){
+      binary += unzip_rest(rest)
+     }
+  
+    return binary;
+  };
+  
+  export const build_url =(data)=>{
+      const Binary_Data = toBinary(data);
+      const Head = calculate_base(Binary_Data.length);
+      return zip_binary(Binary_Data, Head);
+  
   }
-
-  return result || "0"; // Manejo del caso de número 0
-}
-
-export function convertFromBase(str) {
-  const base = seed.length; // Base 64
-
-  let result = 0;
-  for (let i = 0; i < str.length; i++) {
-    const digitValue = seed.indexOf(str[i]);
-    result = result * base + digitValue;
+  
+  
+ export const decode_url  =(url)=>{
+      const BinaryData = unzip_binary(url.replaceAll("*", ""));
+      return binToString(BinaryData)
+  
   }
-
-  return result;
-}
-
-// const binaryToNumber = () => {
-//   parseInt;
-// };
-
-// const numberToBinary = (num) => {
-//   return num.toString(2);
-// };
-
-// const hexaToBinary = () => {
-//   var numeroHexadecimal = "2A"; // Representa el número hexadecimal 2A
-//   var numeroDecimal = parseInt(numeroHexadecimal, 16); // Convierte de hexadecimal a decimal
-//   var numeroBinario = numeroDecimal.toString(2); // Convierte de decimal a binario
-// };
-
-
-// const toBinary =()=>{
-//     var binario = "";
-//     for (var i = 0; i < palabra.length; i++) {
-//         var ascii = palabra.charCodeAt(i).toString(2);
-//         // Asegurarse de que cada caracter sea de 8 bits
-//         binario += '0'.repeat(8 - ascii.length) + ascii;
-//     }
-//     return binario;
-// }
-
-
-// const toHexa=()=>{
-    
-// }
-
-
-// const toInte =()=>{
-    
-// }
-
-
-// const  binToString=()=>{
-//      var palabra = "";
-//     for (var i = 0; i < binario.length; i += 8) {
-//         var caracterBinario = binario.substr(i, 8); // Tomar cada 8 bits
-//         var caracterDecimal = parseInt(caracterBinario, 2); // Convertir a decimal
-//         var caracter = String.fromCharCode(caracterDecimal); // Convertir a carácter
-//         palabra += caracter; // Agregar el carácter a la palabra
-//     }
-//     return palabra;
-
-// }
-
 
 
 genen_seed_zip();
